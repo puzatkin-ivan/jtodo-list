541a0da5b61d1aa0188be17c03094ec1f10b3919
Michael <moskvichevmihail60@yandex.ru>
2019-01-11 01:19:08 +0300

    Добавил ViewController

---
+++ src/main/java/com/jtodo/view/ViewController.java
@@ -0,0 +1,36 @@
+package com.jtodo.view;
+
+import com.jtodo.toDoObjects.IToDoObject;
+
+import java.util.Stack;
+
+public class ViewController implements IViewController {
+    Stack<IToDoObject> objs = new Stack<>();
+
+    @Override
+    public void addToViewer(IToDoObject newObj) {
+        objs.add(newObj);
+    }
+
+    @Override
+    public void deleteLastView() {
+        objs.pop();
+    }
+
+    @Override
+    public IToDoObject getLast() {
+        return objs.peek();
+    }
+
+    @Override
+    public void display() {
+        if(objs.size() > 0) {
+            System.out.println(objs.peek());
+        }
+    }
+
+    @Override
+    public boolean empty() {
+        return objs.empty();
+    }
+}
