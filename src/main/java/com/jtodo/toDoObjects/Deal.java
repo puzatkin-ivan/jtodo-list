ff529e8a391f373265ca1202fb075e1269b9ca08
Michael <moskvichevmihail60@yandex.ru>
2019-01-11 00:28:37 +0300

    Добавил класс Дела

---
+++ src/main/java/com/jtodo/toDoObjects/Deal.java
@@ -0,0 +1,88 @@
+package com.jtodo.toDoObjects;
+
+import com.jtodo.status.IStatus;
+import com.jtodo.status.InProcess;
+import com.jtodo.status.Status;
+
+public class Deal implements IDeal {
+    private String name;
+    private IStatus status;
+
+    public Deal() {
+        name = "Undefined";
+        status = new InProcess();
+    }
+
+    public Deal(String name) {
+        this.name = name;
+        this.status = new InProcess();
+    }
+
+    public Deal(String name, IStatus status) {
+        this.name = name;
+        this.status = status;
+    }
+
+    @Override
+    public IToDoObject openList(int num) {
+        System.out.println("Sorry, but you can't open list here.");
+        return null;
+    }
+
+    @Override
+    public void createList(String listName) {
+        System.out.println("Sorry, but you can't create list here.");
+    }
+
+    @Override
+    public void createDeal(String dealName) {
+        System.out.println("Sorry, but you can't create deal here.");
+    }
+
+    @Override
+    public void deleteList(int listNum) {
+        System.out.println("Sorry, but you can't delete list here.");
+    }
+
+    @Override
+    public void deleteDeal(int dealNum) {
+        System.out.println("Sorry, but you can't delete deal here.");
+    }
+
+    @Override
+    public void renameTo(int num, String newName) {
+    }
+
+    @Override
+    public void changeStatus(int dealNum, IStatus newStatus) {
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public IStatus getStatus() {
+        return status;
+    }
+
+    @Override
+    public String toString() {
+        return name + ": " + status.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Deal) {
+            Deal deal = (Deal) obj;
+            return deal.getName().equals(this.getName());
+        }
+        return false;
+    }
+}
