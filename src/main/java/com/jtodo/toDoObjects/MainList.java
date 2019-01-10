a4f03040a12d93c8260e8b996e3394bb2997196b
Michael <moskvichevmihail60@yandex.ru>
2019-01-11 00:45:15 +0300

    Добавил пару классов для Списка

---
+++ src/main/java/com/jtodo/toDoObjects/MainList.java
@@ -0,0 +1,115 @@
+package com.jtodo.toDoObjects;
+
+import com.jtodo.status.IStatus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainList implements IMainList {
+    final String EMPTY_MSG = "Sorry, but your's todo lists are missing.\nDon't worry, because you can create them right now!";
+    List<IToDoList> lists = new ArrayList<>();
+
+    public MainList() {
+    }
+
+    public MainList(List<IToDoList> lists) {
+        this.lists = lists;
+    }
+
+    @Override
+    public String toString() {
+        String res = "";
+        for(int i = 0; i < lists.size(); i++) {
+            res += (i + 1);
+            res += ". ";
+            res += lists.get(i).getName();
+            res += '\n';
+        }
+
+        if(res.equals("")) {
+            res = EMPTY_MSG;
+        }
+
+        return res;
+    }
+
+    @Override
+    public IToDoObject openList(int num) {
+        num--;
+        if(num < lists.size() && num >= 0) {
+            return lists.get(num);
+        }
+
+        return null;
+    }
+
+    @Override
+    public void createList(String listName) {
+        lists.add(new ToDoList(listName));
+    }
+
+    @Override
+    public void createDeal(String dealName) {
+        System.out.println("Sorry, but you can't create deal here.");
+    }
+
+    @Override
+    public void deleteList(int listNum) {
+        listNum--;
+        if(listNum < lists.size()) {
+            lists.remove(listNum);
+        }
+    }
+
+    @Override
+    public void deleteDeal(int dealNum) {
+        System.out.println("Sorry, but you can't delete deal here.");
+    }
+
+    @Override
+    public void renameTo(int num, String newName) {
+        num--;
+        if(num < lists.size()) {
+            lists.get(num).setName(newName);
+        }
+    }
+
+    @Override
+    public void changeStatus(int dealNum, IStatus newStatus) {
+        System.out.println("Sorry, but you can't change status here.");
+    }
+
+    @Override
+    public void setName(String name) {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public IStatus getStatus() {
+        return null;
+    }
+
+    @Override
+    public List<IToDoList> getLists() {
+        return lists;
+    }
+
+    @Override
+    public void addList(IToDoList list) {
+        lists.add(list);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof MainList) {
+            MainList list = (MainList) obj;
+            return list.getLists().equals(this.getLists());
+        }
+        return false;
+    }
+}
---
+++ src/main/java/com/jtodo/toDoObjects/ToDoList.java
@@ -0,0 +1,117 @@
+package com.jtodo.toDoObjects;
+
+import com.jtodo.status.IStatus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ToDoList implements IToDoList {
+    final String EMPTY_LIST = "Sorry, but your todo list is empty.\nDon't worry, because you can create deals right now!";
+    private String name;
+    private List<IDeal> deals = new ArrayList<>();
+
+    public ToDoList() {
+        name = "Undefined";
+    }
+
+    public ToDoList(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public IToDoObject openList(int num) {
+        System.out.println("Sorry, but you can't open list here.");
+        return null;
+    }
+
+    @Override
+    public void createList(String listName) {
+        System.out.println("Sorry, but you can't create list here.");
+    }
+
+    @Override
+    public void createDeal(String dealName) {
+        deals.add(new Deal(dealName));
+    }
+
+    @Override
+    public void deleteList(int listNum) {
+        System.out.println("Sorry, but you can't delete list here.");
+    }
+
+    @Override
+    public void deleteDeal(int dealNum) {
+        dealNum--;
+        if(dealNum < deals.size()) {
+            deals.remove(dealNum);
+        }
+    }
+
+    @Override
+    public void renameTo(int num, String newName) {
+        IDeal changedDeal = new Deal(newName);
+        num--;
+        deals.remove(num);
+        deals.add(num, changedDeal);
+    }
+
+    @Override
+    public void changeStatus(int dealNum, IStatus newStatus) {
+        dealNum--;
+        IDeal changedDeal = new Deal(deals.get(dealNum).getName(), newStatus);
+        deals.remove(dealNum);
+        deals.add(dealNum, changedDeal);
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public IStatus getStatus() {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        String res = name;
+        res += '\n';
+        for(int i = 0; i < deals.size(); i++) {
+            res += (i + 1);
+            res += ". ";
+            res += deals.get(i).getName();
+            res += ": ";
+            res += deals.get(i).getStatus();
+            res += '\n';
+        }
+        if(res.equals(name + '\n')) {
+            res += EMPTY_LIST;
+        }
+        return res;
+    }
+
+    @Override
+    public List<IDeal> getDeals() {
+        return deals;
+    }
+
+    @Override
+    public void addDeal(IDeal deal) {
+        deals.add(deal);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof ToDoList) {
+            ToDoList list = (ToDoList) obj;
+            return (list.getName().equals(this.getName())) && (list.getDeals().equals(this.getDeals()));
+        }
+        return false;
+    }
+}
